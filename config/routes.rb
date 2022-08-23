e10be51b44f7754720d1dc6e31443d4a1ba47e8b
Vando <vandorramos@gmail.com>
2022-08-23 11:39:55 -0300

    Changing all profiles files for users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+  
   namespace :site do
     get 'welcome/index'
   end
-  namespace :profiles_backoffice do
+  namespace :users_backoffice do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-  devise_for :profiles
+
+  devise_for :users
   devise_for :admins
 
   root to: 'site/welcome#index'
