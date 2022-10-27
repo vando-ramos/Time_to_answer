7a4e87c259f8ed1f42eee6a2af86c3aef72a820f
Vando <vandorramos@gmail.com>
2022-10-27 10:08:21 -0300

    adjusting admins routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :admins_backoffice do
-    get 'admins/index'
-  end
+  
   namespace :users_backoffice do
     get 'welcome/index'
   end
@@ -13,7 +11,8 @@ Rails.application.routes.draw do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
-    get 'welcome/index'
+    get 'welcome/index' # Dashboard
+    resources :admins, only: [:index] # Admins
   end
 
   devise_for :users
