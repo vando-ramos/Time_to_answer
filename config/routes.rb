01c22b6359ce2de2f7838636adaea3b8a643667f
Vando <vandorramos@gmail.com>
2022-11-14 11:33:18 -0300

    creating the controller for the subjects

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
   
-  namespace :users_backoffice do
-    get 'welcome/index'
-  end  
   namespace :site do
     get 'welcome/index'
   end
@@ -12,10 +9,13 @@ Rails.application.routes.draw do
   namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
     resources :admins # Admins
+    resources :subjects # Subjects
   end
 
   devise_for :users
   devise_for :admins
+
+  get 'home', to: 'site/welcome#index'
 
   root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
